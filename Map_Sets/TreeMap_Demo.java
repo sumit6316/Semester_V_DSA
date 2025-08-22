8b7c7fd7963ba7f5d38b4959b35abab8926e7f82
Sumit Sahu <142619166+sumit6316@users.noreply.github.com>
2025-08-22 23:11:18 +0530

    Add files via upload

---
+++ Map_Sets/TreeMap_Demo.java
@@ -0,0 +1,20 @@
+package Hash_Map;
+
+import java.util.*;
+
+public class TreeMap_Demo {
+        public static void main(String[] args) {
+        TreeMap<String,Integer> map = new TreeMap<>();
+        // put
+        map.put("Sumit",89);
+        map.put("Aman",94);
+        map.put("Uday",96);
+        map.put("Pragyan",90);
+        map.put("Shiv",92);
+
+        // map.put("null",93);
+
+        //print
+        System.out.println(map);
+}
+}
